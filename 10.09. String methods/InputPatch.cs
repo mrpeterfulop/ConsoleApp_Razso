ce8f75977976eb7bc03df2a4a58a60e035a398dd
Peter <mrpeterfulop@gmail.com>
2020-10-09 23:15:42 +0200

    09.02. Import from txt file

---
+++ 10.09. String methods/InputPatch.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _10._09.String_methods
+{
+    internal class InputPatch
+    { 
+
+        public string[] extensionFile = new string[] { };
+        bool stopLoop = false;
+
+        public void Patch()
+        {
+
+            extensionFile = File.ReadLines(@"C:\Users\mrpet\Documents\textExtensions.txt").ToArray();
+
+            string patch;
+            string fileName;
+            string fileExtension;
+            string textFile;
+            bool validExt;
+
+            do
+            {
+
+
+                Console.WriteLine("Kérlek add meg a dokumentum elérési útját:");
+                patch = Console.ReadLine();
+                int locateLength = patch.Length;
+
+                int extStart = patch.LastIndexOf(".") + 1;
+                int fileNameStart = patch.LastIndexOf(@"\") + 1;
+
+                int extensionLength = locateLength - extStart;
+                int fileNameLength = locateLength - fileNameStart;
+
+                fileExtension = patch.Substring(extStart, extensionLength);
+                fileName = patch.Substring(fileNameStart, fileNameLength - extensionLength - 1);
+
+                textFile = File.ReadAllText($@"{patch}");
+                validExt = Array.IndexOf(extensionFile, fileExtension) >= 0;
+
+                if (validExt == false)
+                {
+                    Console.WriteLine("A kiterjesztés nem megfelelő!");
+                }
+
+
+            } while (validExt == false);
+
+            stopLoop = true;
+            Console.WriteLine($"\nA file neve: {fileName}");
+            Console.WriteLine($"A file kiterjesztés: {fileExtension}");
+            Console.WriteLine();
+            Console.Write($"A tallózott {fileName} dokumentum tartalma:\n{textFile}");
+
+        }
+
+
+        public void loopPatch()
+        {
+
+            do
+            {
+                try
+                {
+                    Patch();
+                }
+
+                catch (ArgumentOutOfRangeException)
+                {
+
+                    Console.WriteLine("Hiba az elérési útvonalban!");
+                }
+                catch(ArgumentException)
+                {
+                    Console.WriteLine("Érvénytelen karakter!");
+                }  
+
+            } while (stopLoop == false);
+
+        }
+
+    }
+
+    
+}
+
---
+++ 10.09. String methods/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _10._09.String_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //1. Substring metódus
+
+            //Substrin metódus: A zárójelben megadott értéktől kezdi a tömb elemeinek felsorolását. Jelen esetben a string, szöveg tartalmának megjeleíntése az 5. karaktertől kezdődik! Számolás a tömbök logikája alapján, 0-tól! 
+
+            //Kiegészíthető egy második paraméterrel is (5,3) ami alapján az első paraméterrel meghatározott tartalomkezdés a második paraméterben meghatározott paraméterig tart! Jelen esetben 3 karaktert számol még az 5. elemtől!
+
+            /* string str = "Géza kék az ég!";
+             string subStr = str.Substring(6);
+             Console.WriteLine($"Substring={subStr}");*/
+
+            // string word = "alma";
+            // string text = "Ebben a szövegben valahol az alma szó szerepel, alma módra!!";
+
+
+
+            /*
+            Console.WriteLine($"A szöveg a következő:\n{text}\nA keresett szó:\n{word}");
+
+            Console.WriteLine($"A szövegben a(z) {word} szó első előfordulási helye a(z) {text.IndexOf(word)}. indexű elemnél kezdődik!");
+
+            Console.WriteLine($"A szövegben a(z) {word} szó utolsó előfordulási helye a(z) {text.LastIndexOf(word)}. indexű elemnél kezdődik!");
+
+            */
+            /*
+            Console.WriteLine($"A korábbi kifejezés: {word}");
+            
+            Console.WriteLine($"Kérlek add meg az új kifejezést:");
+
+            string newInput = Console.ReadLine();
+
+            word = newInput;
+
+            //string replace = word.Replace(text, newInput);
+
+
+
+            Console.WriteLine($"A megadott kifejezés: {newInput}");
+
+            Console.WriteLine($"A szöveg a következő:\n{text}\nA keresett szó:\n{word}");
+
+            Console.WriteLine($"A szövegben a(z) {word} szó első előfordulási helye a(z) {text.IndexOf(word)}. indexű elemnél kezdődik!");
+
+            Console.WriteLine($"A szövegben a(z) {word} szó utolsó előfordulási helye a(z) {text.LastIndexOf(word)}. indexű elemnél kezdődik!");
+              */
+
+
+
+
+            
+
+
+
+            /*
+            foreach (var item in extensionFile)
+            {
+                Console.Write($"{item},");
+            }
+            */
+
+
+            var inputPatch = new InputPatch();
+
+            inputPatch.loopPatch();
+
+            
+            //Console.WriteLine($"A tömb elemeinek száma:{ extensionFile.Length}");
+            //Console.WriteLine($"A tömb elemei:");
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
