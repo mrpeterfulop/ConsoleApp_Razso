a90488bc513221caf955cda9de841bc1fed80bc1
Peter <mrpeterfulop@gmail.com>
2020-10-22 20:54:50 +0200

    10.22. Fibonacci, update! (use: "string.Join" to list the List items!)

--- 10.21. Fibonacci/WriteConsole.cs
+++ 10.21. Fibonacci/WriteConsole.cs
@@ -13,35 +13,45 @@ namespace _10._21.Fibonacci
         {
             Console.WriteLine($"Az első {input} szám a Fibonacci sorozatból:");
             ColorAlerts.colorGreen();
+            Console.WriteLine(string.Join(",", ListShare.FibonacciList));
+            ColorAlerts.colorWhite();
+
+            /*
             for (int i = 0; i < ListShare.FibonacciList.Count; i++)
             {
                 Console.Write(ListShare.FibonacciList[i] + (i != (ListShare.FibonacciList.Count - 1) ? "," : ""));
             }
-            ColorAlerts.colorWhite();
+            */
 
             Console.WriteLine("\n");
 
             Console.WriteLine($"Az első {input} szám közül {ListShare.FibonacciEven.Count-1} db páros, 1 db nulla:");
 
             ColorAlerts.colorBlue();
+            Console.WriteLine(string.Join(",", ListShare.FibonacciEven));
+            ColorAlerts.colorWhite();
+
+            /*
             for (int i = 0; i < ListShare.FibonacciEven.Count; i++)
             {
                 Console.Write(ListShare.FibonacciEven[i] + (i != (ListShare.FibonacciEven.Count - 1) ? "," : ""));
             }
-            ColorAlerts.colorWhite();
-
-
+            */
 
             Console.WriteLine("\n");
 
             Console.WriteLine($"Az első {input} szám közül {ListShare.FibonacciOdd.Count} db páratlan:");
 
             ColorAlerts.colorRed();
+            Console.WriteLine(string.Join(",", ListShare.FibonacciOdd));
+            ColorAlerts.colorWhite();
+
+            /*
             for (int i = 0; i < ListShare.FibonacciOdd.Count; i++)
             {
                 Console.Write(ListShare.FibonacciOdd[i] + (i != (ListShare.FibonacciOdd.Count - 1) ? "," : ""));
             }
-            ColorAlerts.colorWhite();
+            */
             Console.WriteLine();
         }
 
